62ef56d7b1a881a888f4be733e1283c10af13a04
Chance Nyasulu <96126430+CHANCENY@users.noreply.github.com>
2023-12-28 20:59:24 +0530

    sites folder

---
+++ sites/settings/application/settings.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Setting file have being made to give user access to some of php.ini
+ * settings
+ */
+
+ini_set ('display_errors', 1);
+ini_set ('display_startup_errors', 1);
+error_reporting (E_ALL);
+
+
+
